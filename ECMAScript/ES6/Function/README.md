4658bccee85174d2a7efc2383a0450fda72dea03
pwn <hi@pwn.buzz>
2016-01-13 17:57:57 +0800

    添加函数扩展示例

---
+++ ECMAScript/ES6/Function/README.md
@@ -0,0 +1,40 @@
+该 fiddle 用于演示函数类型（[Function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function）的扩展
+
+```sh
+$ npm install
+$ npm run babel-node
+```
+
+---
+
+形参默认值（不计入 `fn.length` 内）：
+
+- function fn(x, y = 0) // 基本用例
+- function fn({ x, y = 0 }) // 解构赋值
+- function fn({ x, y = 0 } = { x: 0, y: 0 }) // 形参默认值 + 解构赋值默认值
+
+REST 形参：
+
+```js
+function fn(head, ...tail) {}
+let [ m, ...n ] = [ 1, 2, 3 ]
+```
+
+SPREAD 操作符：
+
+```js
+fn(...[ 1, 2, 3 ])
+fn(...[ ...[ 1, 2], 3 ])
+```
+
+箭头函数：
+
+- (args) => { statements }
+- (args) => expression
+  - 等同于 (args) => { return expression }
+- arg => { statements }   // 只有一个形参时可省略圆括号
+- () => { statements }    // 没有形参时不可省略圆括号
+- () => ({ foo: 'bar' })  // 特例：函数体为对象字面量表达式
+- 支持 REST 形参、解构赋值及形参默认值
+- 词法 this 关键字
+- `this` 关键字不再自动绑定而是从外围作用域中继承（`lexical this`）
---
+++ ECMAScript/ES6/Function/index.js
@@ -0,0 +1,135 @@
+//
+// 形参默认值
+//
+
+{
+
+  // 基本用例
+  function f1(x, y = 0) {
+    console.log(`[f1] x:${x}, y:${y}`)
+  }
+  console.log('f1.length:', f1.length) // 形参默认值不计入 fn.length 内
+
+  f1(1, 2)
+  f1(1)
+  f1()
+
+  // 解构赋值
+  function f2({ x, y = 0 }) {
+    console.log(`[f2] x:${x}, y:${y}`)
+  }
+  console.log('f2.length:', f2.length) // 形参默认值不计入 fn.length 内
+
+  f2({ x: 1, y: 2 })
+  f2({ x: 1 })
+  f2({})
+  try {
+    f2()
+  } catch(err) { console.log(err) }
+
+  // 形参默认值 + 结构赋值默认值
+  function f3({ x, y = 0 } = { x: 0, y: 0 }) {
+    console.log(`[f3] x:${x}, y:${y}`)
+  }
+  console.log('f3.length:', f3.length) // 形参默认值不计入 fn.length 内
+
+  f3({ x: 1, y: 2 })
+  f3({ x: 1 })
+  f3({})
+  f3()
+
+}
+
+
+//
+// REST 形参
+//
+
+{
+
+  function fn(head, ...tail) {
+    console.log('head:', head)
+    console.log('tail:', tail)
+  }
+  console.log('fn.length:', fn.length) // REST 形参不计入 fn.length 内
+
+  fn('foo', 'bar', 'baz')
+  fn('foo', 'bar')  // REST 形参永远是一个数组，即使只有一个多余的实参
+  fn('foo')         // REST 形参永远是一个数组，即使没有多余的实参
+  fn()              // REST 形参永远是一个数组，即使没有多余的实参
+
+  // REST「形参」亦可应用于解构赋值解构中
+  let [ m, ...n ] = [ 1, 2, 3 ]
+  console.log('n:', n)
+
+}
+
+
+//
+// SPREAD 操作符
+//
+
+{
+
+  function fn(x, y, z) {
+    console.log(`x: ${x}, y: ${y}, z: ${z}`)
+  }
+
+  // SPREAD 操作符用于「展开」iterable（可以理解为移除两边的方括号）
+
+  // #1：应用于函数调用
+  fn(...[ 1, 2, 3 ])
+
+  // #2：应用于数组字面量
+  fn(...[ ...[ 1, 2], 3 ])
+
+}
+
+
+//
+// 箭头函数
+//
+
+{
+
+  function exec(fn, ...args) { console.log(fn(...args)) }
+
+  // 基本格式
+  exec(
+    (x, y) => {
+      let z = x + y
+      return z
+    },
+    1, 2
+  )
+
+  // 简化格式[1]
+  exec(
+    (x, y) => x + y,  // 等同于 (x, y) => { return x + y }
+    1, 2
+  )
+
+  // 简化格式[2]
+  // - 只有一个形参时可省略圆括号
+  // - 没有形参时不可省略圆括号
+  exec(v => v, 'foobar')
+  exec(() => 'foobar')
+
+  // 特例：函数体为对象字面量表达式
+  exec(() => ({ foo: 'bar' }))
+
+  // this 关键字不再自动绑定而是从外围作用域中继承
+  let obj = {
+    fn1() {
+      return function getThis() {
+        return this
+      }
+    },
+    fn2() {
+      return () => this
+    }
+  }
+  console.assert(!Object.is(obj.fn1()(), obj))
+  console.assert(Object.is(obj.fn2()(), obj))
+
+}
