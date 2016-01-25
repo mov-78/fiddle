e0903a0ad963eb738fb0d12ed09db5d45902f74a
Pwn <hi@pwn.buzz>
2016-01-25 15:48:56 +0800

    更新注释
    Signed-off-by: Pwn <hi@pwn.buzz>

--- React/refs/Function/modules/main.js
+++ React/refs/Function/modules/main.js
@@ -7,8 +7,13 @@ const ReactDOM = require(`react-dom`)
 const AutoFocusInput = require(`./components/AutoFocusInput`)
 
 ReactDOM.render(<AutoFocusInput ref={ function onLoad(component) {
-  // 应用在 ReactElement 上时，ref 回调的参数为其对应的 React.Component 实例
+  // 应用在 ReactElement 上时，ref 回调的参数为其对应的 ReactComponent 实例
   if (component !== null) {
-    component.log() // 可以通过该实例调用暴露的接口
+
+    // 可以通过该实例调用其暴露的指令式接口（Imperative API）
+    component.log()
+
+    // 或通过 ReactDOM.findDOMNode 来获取该实例对应的 DOM 节点
+
   }
 } } />, document.querySelector(`main`))
