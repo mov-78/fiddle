035f08140dbc5b89a04ea09fb42029a53bb9a08f
Pwn <hi@pwn.buzz>
2016-04-02 18:14:09 +0800

    添加 sinon mock 相关示例
    Signed-off-by: Pwn <hi@pwn.buzz>

---
+++ testing/sinon/mocks/README.md
@@ -0,0 +1,49 @@
+该 fiddle 用于演示 [Mocks](http://sinonjs.org/docs/#mocks) 相关接口
+
+```sh
+$ npm install
+$ npm test
+```
+
+---
+
+![sinon-mock.png](https://raw.githubusercontent.com/pwnn/img/master/sinon-mock.png)
+
+---
+
+## 创建 Mock：
+
+- `sinon.mock( obj )`
+
+## 创建 Expectation：
+
+- `mock.expects( method )`
+- `sinon.expectation.create( [method] )`
+
+## Mock 核心接口：
+
+- `mock.expects( method )`
+- `mock.restore()`
+- `mock.verify()`
+
+## Expectation 核心接口：
+
+#### 手动校验：
+
+- `expectation.verify()`
+
+#### 调用次数相关：
+
+- `expectation.exactly( n )`
+- `expectation.once|twice|thrice()`
+- `expectation.atLeast|atMost( n )`
+- `expectation.never()`
+
+#### 实参相关：
+
+- `expectation.withArgs( ...args )`
+- `expectation.withExactArgs( ...args )`
+
+#### 上下文相关：
+
+- `expectation.on( ctxt )`
---
+++ testing/sinon/mocks/test/mock.js
@@ -0,0 +1,104 @@
+'use strict'
+
+const sinon = require( 'sinon' )
+
+//
+// Expectation 核心接口
+//
+
+describe( 'Expectation'
+        , function () {
+
+            let obj
+              , mock
+            beforeEach( function () {
+                          obj = { nop() {} }
+                          mock = sinon.mock( obj )
+                        }
+                      )
+
+
+            //
+            // expectation.exactly( n )
+            // expectation.once|twice|thrice()
+            // expectation.atLeast|atMost( n )
+            // expectation.never()
+            //
+
+            it( '#exactly( n )'
+              , function () {
+                  mock.expects( 'nop' ).exactly( 2 )
+                  obj.nop(); obj.nop()
+                  mock.verify()
+                }
+              )
+            it( '#once()'
+              , function () {
+                  mock.expects( 'nop' ).once()
+                  obj.nop()
+                  mock.verify()
+                }
+              )
+            it( '#twice()'
+              , function () {
+                  mock.expects( 'nop' ).twice()
+                  obj.nop(); obj.nop()
+                  mock.verify()
+                }
+              )
+            it( '#thrice()'
+              , function () {
+                  mock.expects( 'nop' ).thrice()
+                  obj.nop(); obj.nop(); obj.nop()
+                  mock.verify()
+                }
+              )
+            it( '#atLeast|atMost( n )'
+              , function () {
+                  mock.expects( 'nop' ).atLeast( 1 ).atMost( 3 ) // chainable
+                  obj.nop()
+                  mock.verify()
+                }
+              )
+            it( '#never()'
+              , function () {
+                  mock.expects( 'nop' ).never().verify()
+                }
+              )
+
+
+            //
+            // expectation.withArgs( ...args )
+            // expectation.withExactArgs( ...args )
+            //
+
+            it( '#withArgs( ...args )'
+              , function () {
+                  mock.expects( 'nop' ).withArgs( 'foo' )
+                  obj.nop( 'foo', 'bar' )
+                  mock.verify()
+                }
+              )
+            it( '#withExactArgs( ...args )'
+              , function () {
+                  mock.expects( 'nop' ).withExactArgs( 'foo', 'bar' )
+                  obj.nop( 'foo', 'bar' )
+                  mock.verify()
+                }
+              )
+
+
+            //
+            // expectation.on( ctxt )
+            //
+
+            it( '#on( ctxt )'
+              , function () {
+                  mock.expects( 'nop' ).on( mock )
+                  obj.nop.call( mock )
+                  mock.verify()
+                }
+              )
+
+          }
+        )
