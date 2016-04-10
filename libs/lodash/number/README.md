2ae0de295be4f35ae219f1173ceda1d37fbd44fd
Pwn <hi@pwn.buzz>
2016-04-10 11:05:30 +0800

    添加 lodash/number 相关示例
    Signed-off-by: Pwn <hi@pwn.buzz>

---
+++ libs/lodash/number/README.md
@@ -0,0 +1,11 @@
+该 fiddle 用于演示 `lodash/number` 模块
+
+```sh
+$ npm install
+$ npm test
+```
+
+---
+
+- [clamp( number , [lower] , upper )](https://lodash.com/docs#clamp)
+- [inRange( number , [start=0] , end )](https://lodash.com/docs#inRange)
---
+++ libs/lodash/number/test/number.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const _ = require( 'lodash' )
+    , chai = require( 'chai' )
+
+before( chai.should )
+
+describe( 'lodash/number'
+        , function () {
+
+            // clamp( number , [lower] , upper )
+            it( 'clamp'
+              , function () {
+                  _.clamp( 1 , 0 , 5 ).should.equal( 1 )
+                  _.clamp( -5 , 0 , 5 ).should.equal( 0 )
+                  _.clamp( 10 , 0 , 5 ).should.equal( 5 )
+                }
+              )
+
+            // inRange( number , [start=0] , end )
+            it( 'inRange'
+              , function () {
+                  _.inRange( 0 , 0 , 5 ).should.be.true
+                  _.inRange( 1 , 0 , 5 ).should.be.true
+                  _.inRange( 5 , 0 , 5 ).should.be.false
+                }
+              )
+
+          }
+        )
