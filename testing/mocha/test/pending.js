1ed2a55c826c780d8f81fd1aa3d47c45961e7c45
Pwn <hi@pwn.buzz>
2016-06-20 17:30:35 +0800

    #OCD

--- testing/mocha/test/pending.js
+++ testing/mocha/test/pending.js
@@ -2,9 +2,14 @@ const noop = require( 'lodash.noop' )
 
 describe( 'Pending test cases' , function () {
   xit( 'with xit( title , [implementation] )' )
+  it.skip( 'with it.skip( title , [implementation] )' )
   it( 'by omitting the implementation callback' )
 } )
 
-xdescribe( 'Pending a test suite' , function () {
+xdescribe( 'Pending test suites' , function () {
   it( 'with xdescribe( title , implementation )' , noop )
+} )
+
+describe.skip( 'Pending test suites' , function () {
+  it( 'with describe.skip( title , implementation )' , noop )
 } )
