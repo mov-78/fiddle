a1fc6daf746318f34c3827c614133d3d2add2154
Pwn <hi@pwn.buzz>
2017-02-27 15:22:10 +0800

    Shared Behaviours

--- testing/mocha/test/hooks.js
+++ testing/mocha/test/hooks.js
@@ -22,6 +22,8 @@ describe( 'Hooks' , function () {
     // 若存在父级「测试套件」，则在调用该「测试套件」的 afterEach 方法后调用父级「测试套件」里的 afterEach 方法（递归调用）
     afterEach( noop )
 
+    // 同时，每个 beforeEach → it → afterEach 迭代共享同一个上下文对象（this）
+
 } )
 
 
